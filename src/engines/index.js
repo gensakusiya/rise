a67ae79227a9c3dae578c0fc415c144e351ead20
Artyom Prikhodko <homoogenecus@gmail.com>
2016-03-31 19:11:27 +0300

    create core

--- src/engines/index.js
+++ src/engines/index.js
@@ -1,9 +1,56 @@
 'use strict';
 
 import moduleEngine from './module';
+import namespaceEngine from './namespace';
+import serviceEngine from './service';
+import storeEngine from './store';
+import routerEngine from './router';
 
-export let Context = () => {
+import APPCONST from '../const';
+
+const createCore = (opts) => {
+  const namespace = namespaceEngine();
+  const module = moduleEngine();
+  const store = storeEngine();
+  const router = routerEngine(opts.router);
+
+  let engine = {
+      namespace,
+      module,
+      store,
+      router
+    },
+    context;
+
+  engine.service = serviceEngine({
+    getStore(name) {
+      return engine.store.get(name);
+    }
+  });
+
+  context = {
+    getNamespace(name) {
+      return engine.namespace.get(name);
+    },
+    getStore(name) {
+      return engine.store.get(name);
+    },
+    getService(name) {
+      return engine.service.get(name);
+    },
+
+    getConst() {
+      return APPCONST;
+    },
+
+    navigate(url) {
+      return engine.router.navigate(url);
+    }
+  };
+
   return {
-    Module: moduleEngine()
+    context, engine
   };
 };
+
+export {createCore}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 'use strict';
 
-// механизм неймспейсов #
-// механизм модулей #
-// механизм хранилищ #
-// механизм сервисов #
-// механизм маршрутизации
+import createCore from './engines';
 
-let Rise = function () {
+let context, engine;
+
+/**
+ * opts - options object {router: {callback: function, routes: {}}}
+ * opts.router.callback - callback on change routes
+ * opts.router.routes - application routes
+ * opts.pageBox - content page box
+ * opts.appBox - application box
+ * opts.beforeOnRouteChange - pre hook - call before route changed
+ * opts.beforeModuleInit - pre hook - call after route changed but before new module initialized
+ */
+let Rise = function (opts) {
   this.VERSION = '';
+
+  const core = createCore(opts);
+  context = core.context;
+  engine = core.engine;
 };
 
 Rise.prototype = Object.create({
@@ -16,6 +27,32 @@ Rise.prototype = Object.create({
   },
   stop() {
 
+  },
+
+  getNamespace(name) {
+    return engine.namespace.get(name);
+  },
+  getService(name) {
+    return engine.service.get(name);
+  },
+  getStore(name) {
+    return engine.store.get(name);
+  },
+
+  addModule(name, mObj) {
+    return engine.module.add(name, mObj);
+  },
+  addModuleDescription(name, dObj) {
+    return engine.module.addDescription(name, dObj);
+  },
+  addNamespace(name, nObj) {
+    return engine.namespace.add(name, nObj);
+  },
+  addService(name, sObj) {
+    return engine.service.add(name, sObj);
+  },
+  addStore(name, sObj) {
+    return engine.store.add(name, sObj);
   }
 });
 
